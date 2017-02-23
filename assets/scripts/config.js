754b2f70a307f22bbaf0a52173dfd96a17ce00ee
ladypinetree <maureenpine@gmail.com>
2017-02-22 19:34:42 -0500

    Remove extra slash

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://serene-temple-92248.herokuapp.com/',
+    production: 'https://serene-temple-92248.herokuapp.com',
     development: 'http://localhost:4741',
   },
 };
